a41f0ebeb06838132d8832a1a0518b58bf36f875
krackxor <77487814+krackxor@users.noreply.github.com>
2024-03-29 04:59:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ PASSWORD = ""  # redis password
 
 PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+ADMINS = [6510365486]
